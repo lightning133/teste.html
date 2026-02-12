cf0d2c642eeb8fbc3c91ef12ba5d3a370117782c
lightning133 <wesley.rozzi@escola.pr.gov.br>
2026-02-12 09:04:11 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,15 @@
+console.log("hello world!");
+console.log("123456789");
+console.log("Wesley");
+
+let x = 8 + 9;
+console.log(x);
+
+let y = 8;
+let z = 9;
+
+let t = y + z; 
+console.log(t);
+
+console.log("O resultado é:", t);
+console.log("O outro resultado é, x")
