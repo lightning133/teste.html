e38bea982355b1e67b3f2eaf7f227f2672e7e281
lightning133 <wesley.rozzi@escola.pr.gov.br>
2026-03-12 09:19:50 -0300

    Add files via upload

---
+++ brake_continue.js
@@ -0,0 +1,14 @@
+let carros_estacionados=0;
+const limite maximo=4;
+console.log("---iniciando verificacao de vagas---");
+for(let vaga=1; vaga<=10; vaga++){
+    if(vaga===7 || vaga===7)
+{
+    console.log('vaga ${vaga}:[ineterditada]-pulando...');
+} continue}
+carros_estacionados++;
+// console.log('vaga $ {vaga}[livre]-> carro estacionado!(total:${carros_estacionados})');
+if(carros_estacionados=== limite maximo){
+    console.log("---limite atingido!fechar portão---");
+}
+console.log("retrato final:operação de entrada encerrada");
