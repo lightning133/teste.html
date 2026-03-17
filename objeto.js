4c350f9a7a9db19daa7564bed2c6dc3c97f4f8d8
lightning133 <wesley.rozzi@escola.pr.gov.br>
2026-03-17 08:11:01 -0300

    Add files via upload

--- objeto.js
+++ objeto.js
@@ -1,6 +1,20 @@
-let pessoa={nome:"maria",idade:"30"};
-console.log(pessoa.nome);
-console.log(pessoa["idade"]);
-pessoa.profissão="desenvolvedora";
-pessoa.idade=31;
-console.log(pessoa);
+let pessoa1 = {nome: "Maria", idade: 30};
+console.log(pessoa1.nome);
+console.log(pessoa1["idade"]);
+pessoa1.profissão = "Desenvolvedora";
+pessoa1.idade = 31;
+console.log(pessoa1);
+
+let pessoa2 = {nome: "João", idade: 25};
+console.log(pessoa2.nome);
+console.log(pessoa2["idade"]);
+pessoa2.profissão = "Designer";
+pessoa2.idade = 26;
+console.log(pessoa2);
+
+let pessoa3 = {nome: "Ana", idade: 28};
+console.log(pessoa3.nome);
+console.log(pessoa3["idade"]);
+pessoa3.profissão = "Engenheira";
+pessoa3.idade = 29;
+console.log(pessoa3);
